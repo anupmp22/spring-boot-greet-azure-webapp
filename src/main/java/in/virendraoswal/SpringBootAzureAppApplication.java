f4554c3b4de272843ac672e6af9656cb2ba1e83f
anupmp22 <60257067+anupmp22@users.noreply.github.com>
2023-07-03 11:49:19 +0530

    Update SpringBootAzureAppApplication.java

--- src/main/java/in/virendraoswal/SpringBootAzureAppApplication.java
+++ src/main/java/in/virendraoswal/SpringBootAzureAppApplication.java
@@ -23,3 +23,19 @@ class GreetResource {
 		return "Greeting from Azure App Service.";
 	}
 }
+
+@RestController
+public class IndexController implements ErrorController{
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
